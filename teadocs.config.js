82eed2abea56400c9f83f011213655260d37274f
Cyy-yy <Cyy-yy@163.com>
2023-01-11 16:34:34 +0800

    Second Commit

---
+++ teadocs.config.js
@@ -0,0 +1,24 @@
+'use strict';
+const path = require('path')
+
+module.exports = {
+  doc: {
+    name: "测试文档",
+    description: "",
+    version: "",
+    dir: "",
+    outDir: "",
+    staticDir: ""
+  },
+  theme: {
+    dir: "",
+    title: "",
+    headHtml: "",
+    footHtml: "",
+    isMinify: true,
+    rootPath: "/"
+  },
+  nav: {
+    tree: "./tree"
+  }
+}
